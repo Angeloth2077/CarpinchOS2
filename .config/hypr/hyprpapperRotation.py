f24e7ebe165b23e74a08614bee47265e0be541ac
Angel <reysi2003@gmail.com>
2023-04-12 12:09:23 -0400

    Performance improvements, and making the wallpapers run into an infinite loop

--- .config/hypr/hyprpapperRotation.py
+++ .config/hypr/hyprpapperRotation.py
@@ -3,19 +3,23 @@ import time as t
 import os
 import subprocess
 
-wallpapwer_list = os.listdir('./Wallpapers')
+wallpaper_list = os.listdir('/home/angel/.config/hypr/Wallpapers')
 wait_time = 60
-preload_str = 'preload = /home/angel/.config/hypr/Wallpapers/'
-wallpapwer_str = '\nwallpaper = HDMI-A-1, /home/angel/.config/hypr/Wallpapers/'
+path = '/home/angel/.config/hypr/Wallpapers/'
+preload_str = 'preload = ' + path
+wallpaper_str = '\nwallpaper = HDMI-A-1, ' + path
 
+def variate(activated):
+    while True:
+        if activated == True:
+            for img in wallpaper_list:
+                with open('/home/angel/.config/hypr/hyprpaper.conf', 'w') as w:
+                    w.write(preload_str + img + wallpaper_str + img)
+                os.system('hyprpaper & disown')
+                os.system('hyprctl hyprpaper unload '+ path + img)
+                t.sleep(wait_time)
+                os.system('killall hyprpaper')
+        elif activated == False: break
 
-def variate():
-    for img in wallpapwer_list:
-        with open('./hyprpaper.conf', 'w') as w:
-            w.write(preload_str + str(img) + wallpapwer_str + str(img))
-        os.system('hyprpaper & disown')
-        t.sleep(wait_time)
-
-
-if  __name__ == '__main__':
+if __name__ == '__main__':
     variate()
